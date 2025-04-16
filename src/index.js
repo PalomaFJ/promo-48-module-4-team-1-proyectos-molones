045ee5989f9b01ef0f5fd0e53c11d3b0440fc41b
Patrigar10 <patri.garcia.gomez@gmail.com>
2025-04-16 20:07:05 +0200

    datos de prueba

--- src/index.js
+++ src/index.js
@@ -1,81 +1,87 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const server = express();
 
-
 server.use(cors());
 
-const port = 5000
+const port = 5000;
 server.listen(port, () => {
-    console.log("server is runing on http://localhost:" + port);
-})
+  console.log("server is runing on http://localhost:" + port);
+});
 
 const staticServerPath = "./web/dist";
 server.use(express.static(staticServerPath));
 
+server.use("/images", express.static(path.join(__dirname, "images")));
+
 //datos farzos
 const prove = [
-    {
-        photo: "https://weremote.net/wp-content/uploads/2023/04/desarrolladora-usando-chat-gpt-1920x1405.jpg",
-        image: "https://weremote.net/wp-content/uploads/2023/04/desarrolladora-usando-chat-gpt-1920x1405.jpg",
-        name: "Proyectos molones",
-        slogan: "Tu proyecto de confianza",
-        desc: "Bienvenido al Chiquito Ipsum, el generador de texto de relleno para tus diseños de antes de los dolores.",
-        repo: "https://github.com/",
-        demo: "https://github.com/",
-        technologies: "VS HTML CSS",
-        autor: "Patri",
-        job: "Desarrolladora FullStack",
-    },
-    {
-        photo: "https://weremote.net/wp-content/uploads/2023/04/desarrolladora-usando-chat-gpt-1920x1405.jpg",
-        image: "https://weremote.net/wp-content/uploads/2023/04/desarrolladora-usando-chat-gpt-1920x1405.jpg",
-        name: "Proyectos molones",
-        slogan: "Tu proyecto de confianza",
-        desc: "Bienvenido al Chiquito Ipsum, el generador de texto de relleno para tus diseños de antes de los dolores.",
-        repo: "https://github.com/",
-        demo: "https://github.com/",
-        technologies: "VS HTML CSS",
-        autor: "Paloma",
-        job: "Desarrolladora FullStack",
-    },
-    {
-        photo: "https://weremote.net/wp-content/uploads/2023/04/desarrolladora-usando-chat-gpt-1920x1405.jpg",
-        image: "https://weremote.net/wp-content/uploads/2023/04/desarrolladora-usando-chat-gpt-1920x1405.jpg",
-        name: "Proyectos molones",
-        slogan: "Tu proyecto de confianza",
-        desc: "Bienvenido al Chiquito Ipsum, el generador de texto de relleno para tus diseños de antes de los dolores.",
-        repo: "https://github.com/",
-        demo: "https://github.com/",
-        technologies: "VS HTML CSS",
-        autor: "Irma",
-        job: "Desarrolladora FullStackora",
-    },
-    {
-        photo: "https://weremote.net/wp-content/uploads/2023/04/desarrolladora-usando-chat-gpt-1920x1405.jpg",
-        image: "https://weremote.net/wp-content/uploads/2023/04/desarrolladora-usando-chat-gpt-1920x1405.jpg",
-        name: "Proyectos molones",
-        slogan: "Tu proyecto de confianza",
-        desc: "Bienvenido al Chiquito Ipsum, el generador de texto de relleno para tus diseños de antes de los dolores.",
-        repo: "https://github.com/",
-        demo: "https://github.com/",
-        technologies: "VS HTML CSS",
-        autor: "Laura",
-        job: "Desarrolladora FullStack",
-    }
-]
+  {
+    photo: "http://localhost:5000/images/patri.jpg",
+    image:
+      "https://weremote.net/wp-content/uploads/2023/04/desarrolladora-usando-chat-gpt-1920x1405.jpg",
+    name: "Arte Interactivo",
+    slogan: "Preservando el arte a través de la tecnología",
+    desc: "Una plataforma donde los usuarios pueden explorar movimientos artísticos a lo largo de la historia.",
+    repo: "https://github.com/Patrigar10?tab=repositories",
+    demo: "https://github.com/Patrigar10?tab=repositories",
+    technologies: "HTML, CSS, React, Node.js, Express, MySQL",
+    autor: "Patri",
+    job: "Desarrolladora Full Stack",
+  },
+  {
+    photo: "http://localhost:5000/images/paloma.PNG",
+    image:
+      "https://weremote.net/wp-content/uploads/2023/04/desarrolladora-usando-chat-gpt-1920x1405.jpg",
+    name: "Música Viva",
+    slogan: "Fusionando música y tecnología",
+    desc: "Una plataforma donde los artistas pueden subir sus canciones, recibir comentarios de sus seguidores y generar estadísticas sobre el rendimiento de sus canciones.",
+    repo: "https://github.com/PalomaFJ",
+    demo: "https://github.com/PalomaFJ",
+    technologies: "HTML, CSS, React, Node.js, Express, MySQL",
+    autor: "Paloma",
+    job: "Desarrolladora Full Stack",
+  },
+  {
+    photo: "http://localhost:5000/images/irma.jpg",
+    image:
+      "https://weremote.net/wp-content/uploads/2023/04/desarrolladora-usando-chat-gpt-1920x1405.jpg",
+    name: "Espacios Virtuales",
+    slogan: "Construyendo ideas en el mundo digital",
+    desc: "Una plataforma para diseñar planos arquitectónicos en 3D y recorrerlos en realidad aumentada.",
+    repo: "https://github.com/IrmaPineiro",
+    demo: "https://github.com/IrmaPineiro",
+    technologies: "HTML, CSS, React, Node.js, Express, MySQL",
+    autor: "Irma",
+    job: "Desarrolladora Full Stack",
+  },
+  {
+    photo: "http://localhost:5000/images/laura.PNG",
+    image:
+      "https://weremote.net/wp-content/uploads/2023/04/desarrolladora-usando-chat-gpt-1920x1405.jpg",
+    name: "Lau Chef",
+    slogan: "Recetas sabrosas, soluciones digitales",
+    desc: "Un sitio web para compartir recetas, donde los usuarios pueden encontrar y publicar recetas, añadir ingredientes, y valorar platos.",
+    repo: "https://github.com/LauM25",
+    demo: "https://github.com/LauM25",
+    technologies: "HTML, CSS, React, Node.js, Express, MySQL",
+    autor: "Laura",
+    job: "Desarrolladora Full Stack",
+  },
+];
 
 server.get("/api/infoprojects", (req, res) => {
-    if (prove.length === 0) {
-        res.json({
-            status: "error",
-            result: []
-        })
-    } else {
-        res.json({
-            status: "success",
-            result: prove
-        });
-    }
-})
+  if (prove.length === 0) {
+    res.json({
+      status: "error",
+      result: [],
+    });
+  } else {
+    res.json({
+      status: "success",
+      result: prove,
+    });
+  }
+});
