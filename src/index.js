152f2b98ee1c7312087af8919a9734b5050f765a
Irmitate <irmitate@gmail.com>
2025-04-23 12:51:40 +0100

    server.post

--- src/index.js
+++ src/index.js
@@ -38,12 +38,44 @@ async function getConnection() {
 server.get('/api/infoprojects', async (req, res) => {
   const connection = await getConnection();
 
-const query = "SELECT authors.autor, authors.job, authors.photo, projects.name, projects.demo, projects.description, projects.slogan, projects.technologies , projects.image, projects.repo, projects.fk_author FROM authors INNER JOIN projects ON authors.idAuthor = projects.fk_author ;"
-const [projectsResult] = await connection.query(query);
-connection.end();
-res.status(200).json({
-  success: true,
-  result: projectsResult
+  const query = "SELECT authors.autor, authors.job, authors.photo, projects.name, projects.demo, projects.description, projects.slogan, projects.technologies , projects.image, projects.repo, projects.fk_author FROM authors INNER JOIN projects ON authors.idAuthor = projects.fk_author ;"
+  const [projectsResult] = await connection.query(query);
+  connection.end();
+  res.status(200).json({
+    success: true,
+    result: projectsResult
+  });
 });
-}); 
 
+server.post('/api/project', async (req, res) => {
+  const connection = await getConnection();
+  const authorQuerySQL =
+    "INSERT INTO authors (autor, job, photo) VALUES (?, ?, ?)";
+  const [authorResult] = await connection.query(authorQuerySQL, [
+    req.body.autor,
+    req.body.job,
+    req.body.photo,
+  ]);
+  const idNewAuthor = authorResult.insertId;
+  const projectQuerySQL =
+    "INSERT INTO projects (name, demo, description, slogan, technologies, image, repo, fk_author) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+  const [projectResult] = await connection.query(projectQuerySQL, [
+    req.body.name,
+    req.body.demo,
+    req.body.description,
+    req.body.slogan,
+    req.body.technologies,
+    req.body.image,
+    req.body.repo,
+    idNewAuthor,
+  ]);
+
+  connection.end();
+
+  res.status(200).json({
+    success: true,
+    cardURL: "",
+  });
+
+
+});
