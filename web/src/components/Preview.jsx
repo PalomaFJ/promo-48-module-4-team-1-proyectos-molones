bb59bc6945983136165641a31faba1fb4cc0cf64
Irmitate <irmitate@gmail.com>
2025-04-29 15:10:32 +0100

    preview en render

--- web/src/components/Preview.jsx
+++ web/src/components/Preview.jsx
@@ -58,6 +58,19 @@ function Preview({ project }) {
             </a>
           </div>
         </div>
+
+        {isLinkVisible && urlProject.cardURL && (
+          <p className="project__saved-link">
+            ¡Proyecto guardado! Puedes verlo{" "}
+            <a
+              href={urlProject.cardURL}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              aquí
+            </a>
+          </p>
+        )}
       </article>
     </section>
   );
